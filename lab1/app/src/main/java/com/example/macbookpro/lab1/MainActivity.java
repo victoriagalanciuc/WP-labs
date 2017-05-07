821fb6c9fab9eec3d92f749d2b409732a1381404
MacBookPRO <victoria.galanciuc@gmail.com>
2017-05-07 16:14:07 +0300

    added button interaction changing button color on click

--- lab1/app/src/main/java/com/example/macbookpro/lab1/MainActivity.java
+++ lab1/app/src/main/java/com/example/macbookpro/lab1/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.macbookpro.lab1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button button1;
+    boolean check = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = (Button) findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(check){
+                    check = false;
+
+                    button1.setBackgroundColor(0xffdd6895);
+                }
+                else{
+                    check = true;
+
+                    button1.setBackgroundColor(0xff42aaf4);
+                }
+            }
+
+        });
+
+
+
     }
+
+
+
+
 }
