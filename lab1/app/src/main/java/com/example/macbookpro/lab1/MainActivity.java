505e983414c478a57e1fa2760bf1d06f56f7d5ab
MacBookPRO <victoria.galanciuc@gmail.com>
2017-05-07 16:41:22 +0300

    added window interaction on clicking back button text is changed

--- lab1/app/src/main/java/com/example/macbookpro/lab1/MainActivity.java
+++ lab1/app/src/main/java/com/example/macbookpro/lab1/MainActivity.java
@@ -49,6 +49,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onBackPressed() {
+        text.setText("You have pressed back button");
+    }
 }
 
 
