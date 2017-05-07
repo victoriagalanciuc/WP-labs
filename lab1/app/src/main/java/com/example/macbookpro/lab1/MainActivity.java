aa4291bb8d998f7240c8894cfa38622f31bfd6de
MacBookPRO <victoria.galanciuc@gmail.com>
2017-05-07 16:37:08 +0300

    added button interaction changing text color on button click

--- lab1/app/src/main/java/com/example/macbookpro/lab1/MainActivity.java
+++ lab1/app/src/main/java/com/example/macbookpro/lab1/MainActivity.java
@@ -4,11 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    Button button1;
+    Button button1, button2;
+    TextView text;
+
     boolean check = false;
 
     @Override
@@ -17,30 +22,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        text = (TextView) findViewById(R.id.text);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if(check){
+                if (check) {
                     check = false;
 
                     button1.setBackgroundColor(0xffdd6895);
-                }
-                else{
+                } else {
                     check = true;
 
                     button1.setBackgroundColor(0xff42aaf4);
                 }
             }
-
         });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
 
-
+                text.setTextColor(0xffbc8e8b);
+            }
+        });
     }
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
