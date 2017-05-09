5772afc4605ae5cca9bad2da8256131fbd2811d4
MacBookPRO <victoria.galanciuc@gmail.com>
2017-05-09 16:41:27 +0300

    added button that displays dialog box on click

---
+++ lab2/app/src/main/java/com/example/macbookpro/lab2/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.macbookpro.lab2;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(
+                        MainActivity.this);
+                builder.setMessage("This is lab#2 on PW")
+                        .setTitle("Some dialog box")
+                        .setCancelable(false)
+                        .setPositiveButton("Great!",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,
+                                                        int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+
+    }
+}
